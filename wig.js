bd1568cf504f83de19316e8cd1ef2222478dbf79
LoveTanyaTankou <tanyatankou@yahoo.com>
2026-07-30 13:02:19 -0500

    Add WIG_PRODUCTS database for wigs
    Added product database for WIG_PRODUCTS with various collections and textures.

--- wig.js
+++ wig.js
@@ -93,3 +93,101 @@ const COLLECTIONS = {
 let shoppingCart = JSON.parse(
     localStorage.getItem(CART_STORAGE_KEY)
 ) || [];
+
+/*
+====================================================
+PRODUCT DATABASE
+====================================================
+*/
+
+const WIG_PRODUCTS = [
+
+/* ============================
+LAOS
+============================ */
+
+{
+    collection: "laos",
+    texture: "Straight",
+    image: "images/LAOS Hair Images/laos-straight-wig-model.png",
+    product: "images/LAOS Hair Images/laos-straight-wig-product.png",
+    price: 329.99
+},
+
+{
+    collection: "laos",
+    texture: "Body Wave",
+    image: "images/LAOS Hair Images/laos-body-wave-wig-model.png",
+    product: "images/LAOS Hair Images/laos-body-wave-wig-product.png",
+    price: 329.99
+},
+
+{
+    collection: "laos",
+    texture: "Deep Wave",
+    image: "images/LAOS Hair Images/laos-deep-wave-wig-model.png",
+    product: "images/LAOS Hair Images/laos-deep-wave-wig-product.png",
+    price: 349.99
+},
+
+{
+    collection: "laos",
+    texture: "Curly",
+    image: "images/LAOS Hair Images/laos-curly-model.png",
+    product: "images/LAOS Hair Images/laos-curly-wig-product.png",
+    price: 349.99
+},
+
+{
+    collection: "laos",
+    texture: "Kinky Curly",
+    image: "images/LAOS Hair Images/laos-kinky-curly-model.png",
+    product: "images/LAOS Hair Images/laos-kinky-curly-product.png",
+    price: 359.99
+},
+
+{
+    collection: "laos",
+    texture: "Kinky Straight",
+    image: "images/LAOS Hair Images/laos-kinky-straight-wig-model.png",
+    product: "images/LAOS Hair Images/laos-kinky-straight-wig-product.png",
+    price: 349.99
+},
+
+/* ============================
+CAMBODIAN
+============================ */
+
+{
+    collection: "cambodian",
+    texture: "Straight",
+    image: "images/Cambodian Hair Images/cambodian-straight-wig-model.png",
+    product: "images/Cambodian Hair Images/cambodian-straight-wig-product.jpg",
+    price: 299.99
+},
+
+{
+    collection: "cambodian",
+    texture: "Body Wave",
+    image: "images/Cambodian Hair Images/cambodian-body-wave-wig-model.png",
+    product: "images/Cambodian Hair Images/cambodian-body-wave-wig-product.png",
+    price: 299.99
+},
+
+{
+    collection: "cambodian",
+    texture: "Deep Wave",
+    image: "images/Cambodian Hair Images/cambodian-deep-wave-wig-model.png",
+    product: "images/Cambodian Hair Images/cambodian-deep-wave-wig-product.png",
+    price: 319.99
+},
+
+{
+    collection: "cambodian",
+    texture: "Curly",
+    image: "images/Cambodian Hair Images/cambodian-curly-wig-model.png",
+    product: "images/Cambodian Hair Images/cambodian-curly-wig-product.png",
+    price: 319.99
+}
+
+];
