201acf8b74ba96421598e5f9a8304b880ea7a88d
LoveTanyaTankou <tanyatankou@yahoo.com>
2026-07-30 12:47:51 -0500

    Create product database for hair bundles
    Added a product database with details for various hair products.

--- js/products.js
+++ js/products.js
@@ -1,1 +1,94 @@
+/*
+=========================================================
+BIG BOSS BUNDLES
+PRODUCT DATABASE
+=========================================================
+*/
 
+const PRODUCTS = [
+
+{
+    id:1,
+
+    collection:"LAOS",
+
+    texture:"Deep Wave",
+
+    luxury:"Premium Luxury Hair",
+
+    modelImage:
+    "images/LAOS Hair Images/laos-deep-wave-wig-model.png",
+
+    productImage:
+    "images/LAOS Hair Images/laos-deep-wave-wig-product.png",
+
+    price:189.99,
+
+    bestseller:true
+
+},
+
+{
+    id:2,
+
+    collection:"LAOS",
+
+    texture:"Body Wave",
+
+    luxury:"Premium Luxury Hair",
+
+    modelImage:
+    "images/LAOS Hair Images/laos-body-wave-wig-model.png",
+
+    productImage:
+    "images/LAOS Hair Images/laos-body-wave-wig-product.png",
+
+    price:189.99,
+
+    bestseller:false
+
+},
+
+{
+    id:3,
+
+    collection:"Cambodian",
+
+    texture:"Body Wave",
+
+    luxury:"Luxury Collection",
+
+    modelImage:
+    "images/Cambodian Hair Images/cambodian-body-wave-wig-model.png",
+
+    productImage:
+    "images/Cambodian Hair Images/cambodian-body-wave-wig-product.png",
+
+    price:189.99,
+
+    bestseller:false
+
+},
+
+{
+    id:4,
+
+    collection:"Burmese",
+
+    texture:"Straight",
+
+    luxury:"Luxury Collection",
+
+    modelImage:
+    "images/Burmese Hair Images/burmese-straight-wig-model.png",
+
+    productImage:
+    "images/Burmese Hair Images/burmese-straight-wig-product.jpeg",
+
+    price:189.99,
+
+    bestseller:false
+
+}
+
+];
